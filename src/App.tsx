7785da6f08827817344111016e4652cf31097a7d
steven <72620878@continental.edu.pe>
2025-12-14 13:48:58 -0500

    Agregando el scroll top

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import AppRoutes from "./routes/AppRoutes";
 import Header from "./componentes/Header";
 import Footer from "./componentes/Footer";
 
+import ScrollToTop from "./componentes/ScrollToTop";
+
 function App() {
 
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
 
       <div className="pt-[90px]">
